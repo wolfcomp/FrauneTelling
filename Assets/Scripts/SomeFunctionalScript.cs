ced5296dabb9697610a8a584227f5407ad0d78d1
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-01-25 01:12:00 +0100

    Add dictionary without issues for tarot card sentences

---
+++ Assets/Scripts/SomeFunctionalScript.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class SomeFunctionalScript : MonoBehaviour
+{
+    [SerializeField]
+    public List<TarotCardScriptableObject> CardObjects;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TarotCardScriptableObject.cs
@@ -0,0 +1,25 @@
+﻿using AYellowpaper.SerializedCollections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tarot Card", menuName = "Scriptable Objects/Tarot Card/Tarot Card Object")]
+public class TarotCardScriptableObject : ScriptableObject
+{
+    public string Name;
+    public Sprite Sprite;
+    public SerializedDictionary<string, TarotCardTimes> Sentences = new();
+}
+
+[CreateAssetMenu(fileName = "Tarot Card Times", menuName = "Scriptable Objects/Tarot Card/Tarot Card Times Object")]
+public class TarotCardTimes : ScriptableObject
+{
+    public TarotCardSentences Past;
+    public TarotCardSentences Present;
+    public TarotCardSentences Future;
+}
+
+[CreateAssetMenu(fileName = "Tarot Card Sentences", menuName = "Scriptable Objects/Tarot Card/Tarot Card Sentences Object")]
+public class TarotCardSentences : ScriptableObject
+{
+    public string Good;
+    public string Bad;
+}
