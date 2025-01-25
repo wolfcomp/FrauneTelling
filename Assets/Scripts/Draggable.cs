992f98d2c460e07ab4d980466375d013909ee783
Edvard Andreassen <mitori0211@gmail.com>
2025-01-25 16:46:36 +0100

    Cards now fall back to original place
    Cards now snap back to their original slot when dropped somewhere invalid

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -6,9 +6,13 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 {
     [SerializeField] private Canvas Bounds;
 
+    Transform ParentToReturnTo = null;
+
     public void OnBeginDrag(PointerEventData eventData) 
     {
         Debug.Log("Card sure is starting to drag");
+        ParentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -19,6 +23,7 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public void OnEndDrag(PointerEventData eventData) 
     {
         Debug.Log("Card sure stopped dragging");
+        this.transform.SetParent(ParentToReturnTo);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
