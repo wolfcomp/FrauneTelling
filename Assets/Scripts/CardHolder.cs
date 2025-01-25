98345091ec222eda027fed9936d32fcd10e09b55
Alexander Linde Lossius <alexander.lossius@gmail.com>
2025-01-25 12:17:30 +0100

    created card holder prefabs

---
+++ Assets/Scripts/CardHolder.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    public Sprite Sprite;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
